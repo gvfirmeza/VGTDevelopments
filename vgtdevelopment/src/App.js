080d6f0fcb7f90cff06f47aae09b01add21d7c93
GabrielFAlves <gabrielfirmamento2@gmail.com>
2023-06-30 23:46:54 -0300

    posi

--- vgtdevelopment/src/App.js
+++ vgtdevelopment/src/App.js
@@ -5,13 +5,13 @@ import Nav from './components/Nav/Nav';
 function App() {
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           <b>VGT Developments ®</b>
         </p>
       </header>
-      <Nav />
     </div>
   );
 }
