336b6416cf15796cc88f04cba398ca622db474f2
Gustavo Raia <gustavo.raia02@gmail.com>
2023-07-02 17:56:46 -0300

    Carrossel 50% feito.

--- vgtdevelopment/src/components/Portfolio/Portfolio.jsx
+++ vgtdevelopment/src/components/Portfolio/Portfolio.jsx
@@ -1,9 +1,29 @@
+import React from "react"
+import { useKeenSlider } from "keen-slider/react"
+import "keen-slider/keen-slider.min.css"
 import "./Portfolio-Style.css"
 
-const Portfolio = () => (
-        <div className="container-portfolio">
-            <hr className="quebra"/>
-        </div>
-);
-  
+function Portfolio() {
+    const [sliderRef] = useKeenSlider({
+        loop: true,
+    })
+
+    return (
+        <>
+            <h4>PORTFÓLIO</h4>
+            <div className="container-portfolio">
+                <div ref={sliderRef} className="keen-slider custom-slider">
+                    <div className="keen-slider__slide number-slide1">1</div>
+                    <div className="keen-slider__slide number-slide2">2</div>
+                    <div className="keen-slider__slide number-slide3">3</div>
+                    <div className="keen-slider__slide number-slide4">4</div>
+                    <div className="keen-slider__slide number-slide5">5</div>
+                    <div className="keen-slider__slide number-slide6">6</div>
+                </div>
+            </div>
+            <hr className="quebra" />
+        </>
+    )
+}
+
 export default Portfolio;
