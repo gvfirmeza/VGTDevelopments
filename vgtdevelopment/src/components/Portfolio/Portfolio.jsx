ec5fd754045911762db825d96e569af2b182abc6
gvfirmeza <pmvieira@outlook.com>
2023-07-02 16:49:49 -0300

    O que Fazemos. Feito.

---
+++ vgtdevelopment/src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,9 @@
+import "./Portfolio-Style.css"
+
+const Portfolio = () => (
+        <div className="container-portfolio">
+            <hr className="quebra"/>
+        </div>
+);
+  
+export default Portfolio;
