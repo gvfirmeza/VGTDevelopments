5cc9417e66b0c4f23919e369b80ecb11fa395e2f
gvfirmeza <gvfirmeza@gmail.com>
2026-09-21 19:23:01 +0100

    fix: canonical e SEO para dominio novo (vgtdevelopments.com) + sitemap

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,13 @@
+export const GET = async () => {
+	const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://www.vgtdevelopments.com/</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>`;
+	return new Response(xml, {
+		headers: { 'Content-Type': 'application/xml' }
+	});
+};
